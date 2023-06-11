729461b3657b7f297d745c2cce0d02623b80c747
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-06-11 17:00:19 +0200

    fix cs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,14 +2,14 @@
 declare(strict_types=1);
 
 $findRoot = function ($root) {
-  do {
-    $lastRoot = $root;
-    $root = dirname($root);
-    if (is_dir($root . '/vendor/cakephp/cakephp')) {
-      return $root;
-    }
-  } while ($root !== $lastRoot);
-  throw new Exception('Cannot find the root of the application, unable to run tests');
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
+    throw new Exception('Cannot find the root of the application, unable to run tests');
 };
 $root = $findRoot(__FILE__);
 unset($findRoot);
