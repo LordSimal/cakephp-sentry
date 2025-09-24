01f92625bc142459f2829fe1969fd60f9b13d301
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2025-09-24 23:42:41 +0200

    add sentry logging feature (#36)
    add sentry logging feature

---
+++ src/Log/Engines/SentryLog.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace CakeSentry\Log\Engines;
+
+use Cake\Event\EventManager;
+use Cake\Http\Server;
+use Cake\Log\Engine\BaseLog;
+use Psr\Log\LogLevel;
+use Sentry\Logs\Logs;
+use Stringable;
+
+class SentryLog extends BaseLog
+{
+    public bool $logsWillBeFlushed = false;
+
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+
+        // Send the logs to sentry after the client has received the response
+        if (
+            PHP_SAPI !== 'cli' &&
+            function_exists('fastcgi_finish_request') &&
+            method_exists(Server::class, 'terminate')
+        ) {
+            $this->logsWillBeFlushed = true;
+            EventManager::instance()->on('Server.terminate', function (): void {
+                Logs::getInstance()->flush();
+            });
+        }
+    }
+
+    /**
+     * @param string $level
+     * @param \Stringable|string $message
+     * @param array $context
+     * @return void
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     */
+    public function log($level, string|Stringable $message, array $context = []): void
+    {
+        $message = $this->interpolate($message, $context);
+        $message = $this->formatter->format($level, $message, $context);
+
+        $sentryLogger = Logs::getInstance();
+
+        match ($level) {
+            LogLevel::EMERGENCY, LogLevel::ALERT, LogLevel::CRITICAL => $sentryLogger->fatal($message, [], $context),
+            LogLevel::ERROR => $sentryLogger->error($message),
+            LogLevel::WARNING => $sentryLogger->warn($message, [], $context),
+            LogLevel::NOTICE, LogLevel::INFO => $sentryLogger->info($message, [], $context),
+            LogLevel::DEBUG => $sentryLogger->debug($message, [], $context),
+            default => $sentryLogger->trace($message, [], $context),
+        };
+
+        if (!$this->logsWillBeFlushed) {
+            $sentryLogger->flush();
+        }
+    }
+}
