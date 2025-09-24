d240d4245d0bff334c1ed87d4927be3fe04fe9c5
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2025-09-24 23:21:21 +0200

    adjust sentry log feature

--- src/Log/Engines/SentryLog.php
+++ src/Log/Engines/SentryLog.php
@@ -22,7 +22,11 @@ class SentryLog extends BaseLog
         parent::__construct($config);
 
         // Send the logs to sentry after the client has received the response
-        if (function_exists('fastcgi_finish_request') && method_exists(Server::class, 'terminate')) {
+        if (
+            PHP_SAPI !== 'cli' &&
+            function_exists('fastcgi_finish_request') &&
+            method_exists(Server::class, 'terminate')
+        ) {
             $this->logsWillBeFlushed = true;
             EventManager::instance()->on('Server.terminate', function (): void {
                 Logs::getInstance()->flush();
